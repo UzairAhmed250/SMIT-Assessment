1c54fd4e004f2774e79874ad961b9713403a5cb0
uzairahmed250 <uzair2500231@gmail.com>
2025-01-19 14:23:26 +0500

    ABC-Book

---
+++ script.js
@@ -0,0 +1,17 @@
+// const idZ = document.getElementById("Z");
+// const letterCard = document.querySelector('letter-card');
+
+
+
+// idZ.addEventListener('mousedown', () => {
+//     letterCard.classList.add('active');
+// });
+
+// idZ.addEventListener('mouseup', () => {
+//     letterCard.classList.remove('active');
+// });
+
+// function onHandleChange(){
+
+//     document.getElementsByClassName("letter-card").style.cssText = "background: linear-gradient(to top, green, yellow);"
+// }
